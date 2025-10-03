ec21d4e69f06ca43390555bc02fd9f75ffd6564f
Michael Cesare <65109425+MichaelCesare@users.noreply.github.com>
2025-10-03 13:02:46 +0200

    feature(TSK-008): search query throttle

--- src/components/SearchForm/GameNameField.tsx
+++ src/components/SearchForm/GameNameField.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react'
+import React, { useMemo  } from 'react'
 import { useDispatch, useSelector } from "react-redux";
+import throttle from "lodash.throttle";
 
 import type { AppDispatch } from '@/redux/createStore';
 import { searchGames } from '@/features/game/gamesApi';
@@ -9,11 +10,19 @@ export const GameNameField = () => {
   const dispatch = useDispatch<AppDispatch>();
   const pageSize = useSelector(selectPageSize);
 
-  const handleOnChange = useCallback(( newValue: string ) => {
-    dispatch(searchGames(pageSize, newValue));
-  }, [pageSize, dispatch]);
+  // Create a throttled version of the search function
+  const throttledSearch = useMemo(
+    () =>
+      throttle((value: string) => {
+        dispatch(searchGames(pageSize, value));
+      }, 500, 
+      {
+        leading: false, trailing: true,
+      }), // 500ms throttle window (tweak as needed)
+    [dispatch, pageSize]
+  );
 
   return (
-  <input type="text" defaultValue="" onBlur={(e) => handleOnChange(e.target.value)} />
+  <input type="text" defaultValue="" onBlur={(e) => throttledSearch(e.target.value)} />
   )
 }
